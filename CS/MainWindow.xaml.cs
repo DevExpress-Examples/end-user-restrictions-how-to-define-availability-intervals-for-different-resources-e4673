66ab2e558b5b00d50db3899e9c75dbb9f2abeda9
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows;
+using DevExpress.Xpf.Scheduler;
+using DevExpress.XtraScheduler;
+
+namespace SchedulerResourcesAvailabilitiesWpf {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+
+            schedulerControl1.ApplyTemplate();
+            schedulerControl1.Storage.AppointmentChanging += Storage_AppointmentChanging;
+            schedulerControl1.AllowAppointmentCreate += schedulerControl1_AllowAppointmentCreate;
+
+            ResourceFiller.FillResources(schedulerControl1.Storage, 3);
+            schedulerControl1.Start = new DateTime(2012, 1, 1);
+        }
+
+        private void schedulerControl1_AllowAppointmentCreate(object sender, AppointmentOperationEventArgs e) {
+            bool available = ResourcesAvailabilities.IsIntervalAvailableForResource(
+                schedulerControl1.SelectedResource.Id.ToString(),
+                schedulerControl1.SelectedInterval);
+
+            errorInfo.Content = (available ? null : ResourcesAvailabilities.WarningMessage);
+
+            e.Allow = available;
+        }
+
+        private void Storage_AppointmentChanging(object sender, PersistentObjectCancelEventArgs e) {
+            Appointment apt = (Appointment)e.Object;
+
+            bool available = ResourcesAvailabilities.IsIntervalAvailableForResource(
+                apt.ResourceId.ToString(),
+                new TimeInterval(apt.Start, apt.End));
+
+            errorInfo.Content = (available ? null : ResourcesAvailabilities.WarningMessage);
+
+            e.Cancel = !available;
+        }
+    }
+
+    public static class ResourceFiller {
+        public static string[] Users = new string[] { "Sarah Brighton", "Ryan Fischer", "Andrew Miller" };
+        public static string[] Usernames = new string[] { "sbrighton", "rfischer", "amiller" };
+
+        public static void FillResources(SchedulerStorage storage, int count) {
+            ResourceStorage resources = storage.ResourceStorage;
+            storage.BeginUpdate();
+            try {
+                int cnt = Math.Min(count, Users.Length);
+                for (int i = 1; i <= cnt; i++) {
+                    Resource res = resources.CreateResource(Usernames[i - 1]);
+                    res.Caption = Users[i - 1];
+                    resources.Add(res);
+                }
+            }
+            finally {
+                storage.EndUpdate();
+            }
+        }
+    }
+
+    public static class ResourcesAvailabilities {
+        private static DataTable availabilitiesTable = null;
+
+        public static string WarningMessage { get { return "This time interval is not available for this resource."; } }
+
+        public static bool IsIntervalAvailableForResource(string resourceId, TimeInterval timeInterval) {
+            TimeIntervalCollection availabilities = GetAvailabilitiesForResource(resourceId.ToString());
+            bool result = false;
+
+            for (int i = 0; i < availabilities.Count; i++) {
+                if (availabilities[i].Contains(timeInterval)) {
+                    result = true;
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        private static TimeIntervalCollection GetAvailabilitiesForResource(string resourceId) {
+            DataTable table = GetAvailabilitiesTable();
+            DataView view = new DataView(table, string.Format("ResourceId = '{0}'", resourceId), string.Empty, DataViewRowState.CurrentRows);
+            TimeIntervalCollection result = new TimeIntervalCollection();
+
+            for (int i = 0; i < view.Count; i++) {
+                result.Add(new TimeInterval(Convert.ToDateTime(view[i]["StartTime"]), Convert.ToDateTime(view[i]["EndTime"])));
+            }
+
+            return result;
+        }
+
+        private static DataTable GetAvailabilitiesTable() {
+            if (availabilitiesTable == null) {
+                using (OleDbConnection connection = new OleDbConnection(System.Configuration.ConfigurationManager.ConnectionStrings["ResourcesAvailabilitiesConnectionString"].ConnectionString)) {
+                    OleDbCommand selectCommand = new OleDbCommand("SELECT * FROM ResourcesAvailabilities", connection);
+                    OleDbDataAdapter dataAdapter = new OleDbDataAdapter(selectCommand);
+                    availabilitiesTable = new DataTable("AvailabilitiesTable");
+
+                    dataAdapter.Fill(availabilitiesTable);
+                    availabilitiesTable.Constraints.Add("IDPK", availabilitiesTable.Columns["Id"], true);
+
+                    connection.Close();
+                }
+            }
+
+            return availabilitiesTable;
+        }
+    }
+}
---
+++ VB/Helpers.vb
@@ -0,0 +1,156 @@
+﻿Imports System
+Imports System.Windows.Media
+Imports DevExpress.XtraScheduler
+
+Namespace SchedulerResourcesAvailabilitiesWpf
+    Public Class CellColorConverter
+        Inherits System.Windows.Markup.MarkupExtension
+        Implements System.Windows.Data.IMultiValueConverter
+
+        Public Function Convert(ByVal values() As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements System.Windows.Data.IMultiValueConverter.Convert
+            Dim intervalStart As Date = System.Convert.ToDateTime(values(0))
+            Dim intervalEnd As Date = System.Convert.ToDateTime(values(1))
+            Dim resourceId As String = System.Convert.ToString(values(2))
+            Dim defaultBrush As Brush = DirectCast(values(3), Brush)
+
+            If intervalStart > intervalEnd Then
+                Dim temp As Date = intervalStart
+                intervalStart = intervalEnd
+                intervalEnd = temp
+            End If
+
+            Dim available As Boolean = ResourcesAvailabilities.IsIntervalAvailableForResource(resourceId, New TimeInterval(intervalStart, intervalEnd))
+
+            If available Then
+                Return defaultBrush
+            Else
+                Return defaultBrush.SetBrightness(0.85)
+            End If
+        End Function
+
+        Public Function ConvertBack(ByVal value As Object, ByVal targetTypes() As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object() Implements System.Windows.Data.IMultiValueConverter.ConvertBack
+            Throw New NotImplementedException("DebugConverterFormatter::ConvertBack")
+        End Function
+
+        Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
+            Return Me
+        End Function
+    End Class
+
+    Public Class HSBColor
+        Public Property H() As Double
+        Public Property S() As Double
+        Public Property B() As Double
+        Public Property A() As Byte
+
+        Public Shared Function FromColor(ByVal rgbColor As Color) As HSBColor
+            Dim result As New HSBColor()
+
+            ' preserve alpha
+            result.A = rgbColor.A
+
+            ' convert R, G, B to numbers from 0 to 1
+            Dim r As Double = rgbColor.R / 255R
+            Dim g As Double = rgbColor.G / 255R
+
+            Dim b_Renamed As Double = rgbColor.B / 255R
+
+            Dim max As Double = Math.Max(r, Math.Max(g, b_Renamed))
+            Dim min As Double = Math.Min(r, Math.Min(g, b_Renamed))
+
+            ' hue
+            If max = min Then
+                result.H = 0
+            ElseIf max = r Then
+                result.H = (60 * (g - b_Renamed) / (max - min) + 360) Mod 360
+            ElseIf max = g Then
+                result.H = 60 * (b_Renamed - r) / (max - min) + 120
+            Else
+                result.H = 60 * (r - g) / (max - min) + 240
+            End If
+
+            ' saturation
+            If max = 0 Then
+                result.S = 0
+            Else
+                result.S = 1 - min / max
+            End If
+
+            ' brightness
+            result.B = max
+
+            Return result
+        End Function
+
+        Public Function ToColor() As Color
+            Dim result As New Color()
+
+            result.A = Me.A
+
+            Dim hi As Integer = CInt((Math.Floor(Me.H / 60))) Mod 6
+            Dim f As Double = Me.H / 60 - Math.Floor(Me.H / 60)
+
+            Dim p As Double = Me.B * (1 - Me.S)
+            Dim q As Double = Me.B * (1 - f * Me.S)
+            Dim t As Double = Me.B * (1 - (1 - f) * Me.S)
+
+            Select Case hi
+                Case 0
+                    result.R = CByte(Me.B * 255)
+                    result.G = CByte(t * 255)
+                    result.B = CByte(p * 255)
+                Case 1
+                    result.R = CByte(q * 255)
+                    result.G = CByte(Me.B * 255)
+                    result.B = CByte(p * 255)
+                Case 2
+                    result.R = CByte(p * 255)
+                    result.G = CByte(Me.B * 255)
+                    result.B = CByte(t * 255)
+                Case 3
+                    result.R = CByte(p * 255)
+                    result.G = CByte(q * 255)
+                    result.B = CByte(Me.B * 255)
+                Case 4
+                    result.R = CByte(t * 255)
+                    result.G = CByte(p * 255)
+                    result.B = CByte(Me.B * 255)
+                Case 5
+                    result.R = CByte(Me.B * 255)
+                    result.G = CByte(p * 255)
+                    result.B = CByte(q * 255)
+            End Select
+
+            Return result
+        End Function
+    End Class
+
+    Public Module HelperExtensions
+        <System.Runtime.CompilerServices.Extension> _
+        Public Function SetBrightness(ByVal original As Brush, ByVal brightness As Double) As Brush
+            If brightness < 0 OrElse brightness > 1 Then
+                Throw New ArgumentOutOfRangeException("brightness", "brightness should be between 0 and 1")
+            End If
+
+            Dim result As Brush
+
+            If TypeOf original Is SolidColorBrush Then
+                Dim hsb As HSBColor = HSBColor.FromColor(CType(original, SolidColorBrush).Color)
+                hsb.B = brightness
+                result = New SolidColorBrush(hsb.ToColor())
+            ElseIf TypeOf original Is GradientBrush Then
+                result = original.Clone()
+                ' change brightness of every gradient stop
+                For Each gs As GradientStop In CType(result, GradientBrush).GradientStops
+                    Dim hsb As HSBColor = HSBColor.FromColor(gs.Color)
+                    hsb.B = brightness
+                    gs.Color = hsb.ToColor()
+                Next gs
+            Else
+                result = original.Clone()
+            End If
+
+            Return result
+        End Function
+    End Module
+End Namespace
